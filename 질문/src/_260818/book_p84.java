3a9cf41f90ad57b7207a0fc0870a4c6d29429608
wwwalohacampus <h850415@naver.com>
2026-08-18 10:21:33 +0900

    질문

---
+++ 질문/src/_260818/book_p84.java
@@ -0,0 +1,10 @@
+package _260818;
+
+public class book_p84 {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        System.out.println("변수의 값 : " + a);
+        // a = null;
+    }
+}
